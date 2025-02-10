42d2255d5282d537463b68fdb2f6e7daa5f154a5
Elliott Watson <ElliottWatson0@gmail.com>
2025-02-10 15:34:23 -0500

    made changes to home page /

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,10 +5,10 @@ export function NavBar() {
   return (
     <nav className='navbar'>
       <div className='navbar-brand'>
-        <Link to='/'>Anime Finder</Link>
+        <Link to='/Anime-Site'>Anime Finder</Link>
       </div>
       <div className='navbar-links'>
-        <Link to='/' className='nav-link'>Home</Link>
+        <Link to='/Anime-Site' className='nav-link'>Home</Link>
         <Link to='/favorites' className='nav-link'>Favorites</Link>
       </div>
     </nav>
